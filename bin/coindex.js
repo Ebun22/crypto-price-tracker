999895d30b58a93dd93b09ccbcaf55d77223c5b7
Ebun22 <subeydavid22@gmail.com>
2024-06-13 21:51:21 +0100

    added ASCII art

--- bin/coindex.js
+++ bin/coindex.js
@@ -1,7 +1,28 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
-import pkg from '../package.json' assert { type: 'json' };;
+import pkg from '../package.json' assert { type: 'json' };
+import color from 'colors';
+import figlet from 'figlet'
+
+    figlet.text(
+        "  CRYPTO PRICE TRACKER  ",
+        {
+            font: 'standard',
+            width: 100,
+            horizontalLayout: "default",
+            verticalLayout: "default",
+            whitespacebreak: true,
+        },
+        function (err, data) {
+            if (err) {
+              console.log("Something went wrong...");
+              console.dir(err);
+              return;
+            }
+            console.log(data.green);
+          }
+    );
 
 program
     .version(pkg.version)
